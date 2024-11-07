b07c46352ce047cd7c8198560257804ac981e079
Choll Sak <koonnew8688.cookie@gmail.com>
2024-11-08 05:36:11 +0700

    das

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,10 @@
 from flask import Flask, jsonify, request
 from pymongo import MongoClient
+from flask_socketio import SocketIO, emit
 import logging
+from datetime import datetime
+import time
+from flask_cors import CORS
 
 # Configure logging
 logging.basicConfig(
@@ -10,6 +14,8 @@ logging.basicConfig(
 )
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for the Flask app
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 # MongoDB connection details
 MONGO_URI = "mongodb+srv://pretopgun:pretopgun@pre-topgun-rally-cluste.rojip.mongodb.net/?retryWrites=true&w=majority&appName=pre-topgun-rally-cluster"
@@ -30,7 +36,7 @@ def get_students():
     students = list(collection.find({}, {'_id': 0}))  # Exclude MongoDB's ObjectId from response
     if not students:
         app.logger.info("No students found")
-        return jsonify({"message": "No students found"}), 404  # You can choose a different status code if needed
+        return jsonify({"message": "No students found"}), 404
     return jsonify(students)
 
 @app.route('/students', methods=['POST'])
@@ -39,10 +45,25 @@ def add_student():
     app.logger.info(f"POST request received at /students with data: {student_data}")
     try:
         collection.insert_one(student_data)
+        # Emit a real-time update to connected clients after adding a student
+        socketio.emit('new_student', {'message': 'New student added', 'student': student_data})
         return jsonify({"message": "Student added successfully"}), 201
     except Exception as e:
         app.logger.error(f"Error adding student: {e}")
         return jsonify({"error": str(e)}), 500
 
+@socketio.on('connect')
+def handle_connect():
+    print('Client connected')
+
+@socketio.on('message')
+def handle_message(data):
+    print('Received message:', data)
+    for i in range(10):
+        now = datetime.now()
+        socketio.emit('response', f'You sent {data} {i} {str(now)}')
+        time.sleep(5)
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    # Run the app with SocketIO
+    socketio.run(app, host="0.0.0.0", port=5000, debug=True, use_reloader=False)
