e656755d3d7881d033c517a5b03e868a3b683b16
Choll Sak <koonnew8688.cookie@gmail.com>
2024-11-08 21:49:50 +0700

    fixed

--- backend/app.py
+++ backend/app.py
@@ -14,8 +14,6 @@ logging.basicConfig(
 )
 
 app = Flask(__name__)
-CORS(app)  # Enable CORS for the Flask app
-socketio = SocketIO(app, cors_allowed_origins="*")
 
 # MongoDB connection details
 MONGO_URI = "mongodb+srv://pretopgun:pretopgun@pre-topgun-rally-cluste.rojip.mongodb.net/?retryWrites=true&w=majority&appName=pre-topgun-rally-cluster"
@@ -45,25 +43,12 @@ def add_student():
     app.logger.info(f"POST request received at /students with data: {student_data}")
     try:
         collection.insert_one(student_data)
-        # Emit a real-time update to connected clients after adding a student
-        socketio.emit('new_student', {'message': 'New student added', 'student': student_data})
         return jsonify({"message": "Student added successfully"}), 201
     except Exception as e:
         app.logger.error(f"Error adding student: {e}")
         return jsonify({"error": str(e)}), 500
 
-@socketio.on('connect')
-def handle_connect():
-    print('Client connected')
-
-@socketio.on('message')
-def handle_message(data):
-    print('Received message:', data)
-    for i in range(10):
-        now = datetime.now()
-        socketio.emit('response', f'You sent {data} {i} {str(now)}')
-        time.sleep(5)
 
 if __name__ == "__main__":
     # Run the app with SocketIO
-    socketio.run(app, host="0.0.0.0", port=5000, debug=True, use_reloader=False)
+    app.run(host="0.0.0.0", port=5000, debug=True, use_reloader=False)
